627e15d48bbf421a2ae37e7b77e930caf96d1976
pujitha-321 <pujithamayakunta16@gmail.com>
2026-05-29 11:44:17 +0530

    Add files via upload

---
+++ linearsearch.java
@@ -0,0 +1,26 @@
+public class linearsearch{
+public static void main(String[] args){
+ int[] arr = {20,30,40,50,60,70};
+ int target = 60;
+ for(int i=0;i<=arr.length;i++){
+    if(target == arr[i]){
+        System.out.println("Target is found at "+i);
+        break;
+    }
+ }
+
+}
+}
+    
+
+
+
+
+
+
+
+
+
+    
+    
+
