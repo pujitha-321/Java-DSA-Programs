42a5bc4190dd92d08f5c20ec2186b181f53d1bef
Pujitha <pujithamayakunta16@gmail.com>
2026-05-30 16:56:08 +0530

    Add files via upload

---
+++ fibinocci.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class fibinocci {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.print("Enter the n value : ");
+          int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while(count<= n){
+             int temp = b;
+             b = b+a;
+             a = temp;
+             count++;
+        }
+      System.out.println(b);
+    }
+
+}
