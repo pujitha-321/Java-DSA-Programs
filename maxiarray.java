a0bff1ec9fab2d8bac5a6d1bba603f6fde33a862
pujitha-321 <pujithamayakunta16@gmail.com>
2026-05-30 16:07:36 +0530

    Add files via upload

---
+++ maxiarray.java
@@ -0,0 +1,14 @@
+public class maxiarray {
+    public static void main(String[] args){
+      int[] arr = {10,20,30,7,90,50};
+      int maxi =0;
+      for(int i=0;i<arr.length;i++){
+           int curr = arr[i];
+           if(curr > maxi){
+              maxi = curr;
+           }
+      }
+        System.out.println(maxi);
+    }
+    
+}
