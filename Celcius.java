3f72b32b05f9699925625709de52257f4a5e78a2
Pujitha <pujithamayakunta16@gmail.com>
2026-06-01 19:15:50 +0530

    Update and rename practice1.java to Celcius.java

---
+++ Celcius.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Celcius {
+    public static void main(String[] args){
+
+     Scanner sc = new Scanner(System.in);
+     double celcius,fahrenheit;
+     System.out.print("Enter Celcius Temparature : ");
+     celcius = sc.nextDouble();
+     fahrenheit = (celcius*(9.0/5.0))+32;
+     System.out.println("The Fahrenheit temparature : "+ fahrenheit);
+
+
+
+
+
+
+
+
+
+    }
+    
+}
