6c479fed64479885fb60f8e48d08dcdf86c503b1
Pujitha <pujithamayakunta16@gmail.com>
2026-06-01 19:21:55 +0530

    Update and rename practice2.java to calculator.java

---
+++ calculator.java
@@ -0,0 +1,25 @@
+class calculator{
+public static void main(String[] args){
+    //Grade CAlculator
+     int marks;
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the marks: ");
+marks = sc.nextInt(); 
+if(marks>=95){
+    System.out.println("A Grade");
+}
+else if(marks>=75){
+    System.out.println("B Grade");
+
+}
+else if(marks>=50){
+    System.out.println("C Grade");
+}
+else{
+    System.out.println("Fail");
+}
+}
+}
+
+
+
