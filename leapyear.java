8723778bb6dfdae684657c6208474dc8dcf59fe5
Pujitha <pujithamayakunta16@gmail.com>
2026-06-02 21:45:42 +0530

    Update and rename practice2.java to leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,43 @@
+class leapyear{
+public static void main(String[] args){
+   
+//Leap year checking
+int number;
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the number : ");
+number = sc.nextInt();
+if(number%4 == 0){
+    System.out.println("It is a leap Year");
+
+}
+else{
+    System.out.println("It is not a leap year");
+}
+}
+}
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
