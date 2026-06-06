df98e45b570f9bba31f2dfae3ce6cc9ee07a7d4b
Pujitha <pujithamayakunta16@gmail.com>
2026-06-06 20:11:58 +0530

    Add files via upload

---
+++ patterns.java
@@ -0,0 +1,74 @@
+public class patterns{
+    public static void main(String[] args){
+    int n = 5;
+    /*for(int i=1;i<=n;i++){
+      for(int j=1;j<=n;j++){
+        System.out.print("*");
+
+      }
+      System.out.println();
+    }
+  }
+}*/
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+      System.out.print("*");
+    }
+   System.out.println();
+
+
+  }
+}
+}
+  /*int i;
+  for( i=1;i<=n;i++){
+     for(int j=1;j<=(n-i+1);j++){
+     System.out.print("*");
+     }
+    System.out.println();
+
+
+  }
+*/
+/*for(int i=1;i<=n;i++){
+    int spaces=i-1;
+    for(int k=1;k<=spaces;k++){
+      System.out.print(" ");
+    }
+    int stars = n-i+1;
+    for(int j=1;j<=stars;j++){
+     System.out.print("*");
+
+    }
+    System.out.println();
+    */
+    //Numerical patternn
+    /*for(int i=1;i<=5;i++){
+      
+       for(int j=1;j<=i;j++){
+        System.out.print(j+" ");
+        }
+
+        System.out.println();
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+    }
+  
+    }
+*/
+
+      
+
+
+    
