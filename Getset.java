f80b99fcff4c2b2cddd1051954204a5f8af1d809
Pujitha <pujithamayakunta16@gmail.com>
2026-06-11 20:43:53 +0530

    Add files via upload

---
+++ Getset.java
@@ -0,0 +1,20 @@
+public class Getset {
+    public static void main(String[] args){
+
+     department student1 = new department();
+     student1.AIMl(4500);
+     System.out.println(student1.fees);
+
+
+    }
+}
+class department{
+    int deptid;
+    String deptname;
+    int fees;
+
+    public void AIMl(int b){
+        this.fees = b;
+    }
+
+}
