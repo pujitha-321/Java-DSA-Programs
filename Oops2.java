51874c50fe7f7b1ae78c27b3d84d7633fe8208ea
Pujitha <pujithamayakunta16@gmail.com>
2026-06-11 20:24:50 +0530

    Add files via upload

---
+++ Oops2.java
@@ -0,0 +1,15 @@
+public class Oops2 {
+    public static void main(String[] args){
+      CSE student1 = new CSE();
+      student1.another (30);
+      System.out.println(student1.add);
+    }
+}
+class CSE{
+    int no;
+    String name;
+    int add;
+    public void another( int m){
+        this.add = m;
+    }
+}
