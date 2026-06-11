c8a087aa8afdf359d3e5096b219383a4c3d300e5
Pujitha <pujithamayakunta16@gmail.com>
2026-06-11 20:49:47 +0530

    Update Getset.java

--- Getset.java
+++ Getset.java
@@ -1,10 +1,10 @@
-//THIS IS PROGRAM USING GET AND SET ATTRIBUTES
+//THIS IS PROGRAM USING GET ATTRIBUTES
 public class Getset {
     public static void main(String[] args){
 
      department student1 = new department();
      student1.AIMl(4500);
-     System.out.println(student1.fees);
+     System.out.println(student1.get(fees));
 
 
     }
@@ -17,5 +17,7 @@ class department{
     public void AIMl(int b){
         this.fees = b;
     }
+    public int getfees(){
+        return this.fees;
 
 }
