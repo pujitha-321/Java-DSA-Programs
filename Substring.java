3b0af8d89b4bf58451a20549e78db9c5cfb261b1
Pujitha <pujithamayakunta16@gmail.com>
2026-06-14 20:41:42 +0530

    Add files via upload

---
+++ Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+    public static void main(String[] args){
+        String str = "Hello , World";
+        String sub = str.substring(0,5);
+        System.out.println("The Substring: "+sub);
+        }
+    
+}
