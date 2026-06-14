22700a4e745fc332f61da71913ac6d42bb736518
Pujitha <pujithamayakunta16@gmail.com>
2026-06-14 20:39:47 +0530

    Add files via upload

---
+++ shortestpath.java
@@ -0,0 +1,28 @@
+public class shortestpath {
+    public static void main(String[] args){
+     String str = "WESNNEEWW";
+     int x = 0;
+     int y = 0;
+     for(int i=0;i<str.length();i++){
+      char current = str.charAt(i);
+    if(current == 'W'){
+        x--;
+      }
+     else if(current == 'E'){
+       x++;
+     }
+     else if(current == 'N'){
+        y++;
+     }
+     else if(current == 'S'){
+        y--;
+     }
+     }
+     double distance = Math.sqrt(y*y - x*x);
+     System.out.println("The shortest distance is: "+distance);
+     }
+
+
+    }
+    
+
