3c444371196f500de9708dcbcfd05be257c90d8d
Pujitha <pujithamayakunta16@gmail.com>
2026-06-15 20:29:54 +0530

    Add files via upload

---
+++ convertlower.java
@@ -0,0 +1,8 @@
+public class convertlower {
+    public static void main(String[] args){
+        String lowercase = "poojitha";
+        String uppercase = lowercase.toUpperCase();
+        System.out.println("The uppercase of the string is: "+uppercase);
+    }
+    
+}
