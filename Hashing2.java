f1785755705b544229fc14a3c8cd49db2ffde0aa
Pujitha <pujithamayakunta16@gmail.com>
2026-06-19 21:12:19 +0530

    Add files via upload

---
+++ Hashing2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Hashing2{
+    public static void main(String[] args){
+        
+  Map<Integer,String>mp = new HashMap<>();
+  
+  //USING CONTAINS KEY() METHOD
+
+        System.out.println(mp);
+        mp.put(1,"Krishna");
+        mp.putIfAbsent(1,"Radha");
+        mp.put(2,"Bala");
+        System.out.println(mp);
+
+        //USING REMOVE() METHOD
+        
+        mp.put(1,"Krishna");
+        mp.put(2,"Radha");
+        mp.put(3,"Bala");
+    
+
+        System.out.println(mp);
+        mp.remove(2);
+        System.out.println(mp);
+
+       
+    }
+}
