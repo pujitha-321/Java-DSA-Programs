20f667605bab0960fbe46f820ce97bfa700c6461
Pujitha <pujithamayakunta16@gmail.com>
2026-06-20 21:39:40 +0530

    Add files via upload

---
+++ problem2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class problem2 {
+    public static void main(String[] args){
+        // Intersection of two arrays
+        int[] arr1 = {10,15,20,30,40};
+        int[] arr2 = {10,11,15,25};
+        Set<Integer> st = new HashSet<>();
+        
+            for(int i=0;i<arr1.length;i++){
+                st.add(arr1[i]);
+            }
+            for(int i=0;i<arr2.length;i++){
+                int ce = arr2[i];
+                if(st.contains(ce)){
+                 System.out.print(ce+" ");
+                }
+
+            }  
+        }
+    }
+
